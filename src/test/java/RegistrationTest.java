4999331eb05222aec8d8d5a1cb4d9423952b3ebb
Nikita Kuznetsov <iki19955@ya.ru>
2022-11-02 12:11:31 +0300

    selenide5

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -8,11 +8,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
+
 public class RegistrationTest {
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @BeforeEach
     void openWebPage() {
         open("http://localhost:9999");
@@ -21,49 +25,48 @@ public class RegistrationTest {
     @Test
     void shouldArrangeDelivery() {
         $("[data-test-id='city'] input").setValue("Москва");
-        String dateTest = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id= 'date'] input").setValue(dateTest);
+        String planningDate = generateDate(3);
+        $("[data-test-id= 'date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Кузнецов Никита");
         $("[data-test-id='phone'] input").setValue("+79647989765");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        //$$("button").find(exactText("Забронировать")).click();
         $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible);
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        $(byText(dateTest)).shouldBe(visible);
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
     @Test
     void shouldSelectACityFromTheList() {
         $("[data-test-id='city'] input").setValue("Мо");
         $(withText("Москва")).click();
-        String dateTest = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id= 'date'] input").setValue(dateTest);
+        String planningDate = generateDate(3);
+        $("[data-test-id= 'date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Кузнецов Никита");
         $("[data-test-id='phone'] input").setValue("+79647989765");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible);
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        $(byText(dateTest)).shouldBe(visible);
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
     @Test
     void shouldSelectDate() {
-        String dateTest = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String planningDate = generateDate(3);
         $("[data-test-id='city'] input").setValue("Москва");
         $(withText("Москва")).click();
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(dateTest);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Кузнецов Никита");
         $("[data-test-id='phone'] input").setValue("+79647989765");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible);
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        $(byText(dateTest)).shouldBe(visible);
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
